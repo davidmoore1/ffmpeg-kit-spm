df1593ac2cf1cf6e2e7265ebe23966fdaef86c65
davidmoore1 <david_moore1@sil.org>
2025-01-28 10:58:25 -0500

    Creating release for min.v5.1.2.7

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let release = "min.v5.1.2.6"
+let release = "min.v5.1.2.7"
 
-let frameworks = []
+let frameworks = ["ffmpegkit": "0d3291b86b785b1602d5b446e775fce4d2cc8918b6f8a5fa1c1004bab878340c", "libavcodec": "0f2a90827ef82fbcdf01d338f5fe3876adebba6b6b4f2b19b9827970e05a59cb", "libavdevice": "d64df47dd2e8d66709fbb3f9df8c1b6c704b818b57f4448c072fc7d7e7824140", "libavfilter": "050e8b84c4c7659a8685ec078a69a3ad796bbf732eb517baebc2a2597aaba996", "libavformat": "1d758ea368cf38d4c83a1e5d2024820b77ac7ba2170e4c5b8d6f169c4c6ae3b2", "libavutil": "d50c917198e9cc24f68052571a2a02f098cb6e2e86ac41652c81e14941faf033", "libswresample": "d3665c6f3189a01b6ee09a356caf60596cedea06e9f0cb83568170766ccd4d97", "libswscale": "202fef8a54a14397601244b6666f32bd9dc926de26dad9fbea7680ed80af022d"]
 
 func xcframework(_ package: Dictionary<String, String>.Element) -> Target {
     let url = "https://github.com/davidmoore1/ffmpeg-kit-spm/releases/download/\(release)/\(package.key).xcframework.zip"
